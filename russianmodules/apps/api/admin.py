febf09f924086eaa657c65f1941a0f00bfbc79c3
Arthur Barrett <abarrett@fas.harvard.edu>
2018-08-06 15:04:43 -0400

    Updated lemma admin to show related inflection models.

--- russianmodules/apps/api/admin.py
+++ russianmodules/apps/api/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from .models import Lemma, Inflection
 
-@admin.register(Lemma)
-class LemmaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'external_id', 'lemma', 'translation', 'pos', 'pos_subtype', 'animacy', 'count','level', 'rank', 'aspect', 'transitivity')
-    list_filter = ('level', 'pos', 'pos_subtype', 'animacy', 'aspect', 'transitivity')
-    search_fields = ['id', 'lemma', 'translation']
-    ordering = ['-count', 'level']
+class InflectionInline(admin.TabularInline):
+    model = Inflection
 
 @admin.register(Inflection)
 class InflectionAdmin(admin.ModelAdmin):
@@ -14,3 +10,11 @@ class InflectionAdmin(admin.ModelAdmin):
     list_filter = ('category',)
     autocomplete_fields = ('lemma', )
     search_fields = ['form']
+
+@admin.register(Lemma)
+class LemmaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'lemma', 'translation', 'pos', 'pos_subtype', 'animacy', 'count','level', 'rank', 'aspect', 'transitivity')
+    list_filter = ('level', 'pos', 'pos_subtype', 'animacy', 'aspect', 'transitivity')
+    search_fields = ['id', 'lemma', 'translation']
+    ordering = ['-count', 'level']
+    inlines = [InflectionInline, ]
