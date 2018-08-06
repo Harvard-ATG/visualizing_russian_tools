e3c7eacec3e2356a5478ea690bdb76add2e75010
Arthur Barrett <abarrett@fas.harvard.edu>
2018-08-06 14:39:53 -0400

    added gzip middleware to compres json responses.

--- russianmodules/settings/base.py
+++ russianmodules/settings/base.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'russianmodules.urls'
