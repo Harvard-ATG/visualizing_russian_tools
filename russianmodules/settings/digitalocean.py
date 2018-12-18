f43833df72d9aac5cc73005ae32f6514e6e4af7f
Arthur Barrett <abarrett@fas.harvard.edu>
2018-12-18 11:10:04 -0500

    Added django settings module for digital ocean.

---
+++ russianmodules/settings/digitalocean.py
@@ -0,0 +1,22 @@
+from .local import *
+
+DEBUG = True
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'uGACpvzrJxtkJnxz7HaRYnHnANwHudehmQeDAgcrw8uk2vtBZu8bY4HtMeTqtD8Q')
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database', 'default.sqlite3'),
+    },
+    'russian': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database', 'russian.sqlite3'),
+    }
+}
+
+DATABASE_ROUTERS = ['russianmodules.database_routers.RussianRouter']
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
