3e0bab4000879c8a0953b22ad7436e211064b50c
Arthur Barrett <abarrett@fas.harvard.edu>
2019-02-19 17:09:37 -0500

    Update base.py

--- visualizing_russian_tools/settings/base.py
+++ visualizing_russian_tools/settings/base.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(ROOT_DIR, "static") ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(ROOT_DIR, 'http_static')
 
 # Logging
