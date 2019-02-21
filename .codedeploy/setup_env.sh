8afd103293e9e21bdbbf76221ba9e667a859af89
Arthur Barrett <abarrett@fas.harvard.edu>
2019-02-21 17:57:32 -0500

    Updated codedeploy script to set permissions.

--- .codedeploy/setup_env.sh
+++ .codedeploy/setup_env.sh
@@ -5,6 +5,6 @@ cd /home/ubuntu/sites/visualizing_russian_tools
 echo DJANGO_SETTINGS_MODULE="visualizing_russian_tools.settings.aws" >> .env
 echo DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY >> .env
 echo DJANGO_ENV=$DEPLOYMENT_GROUP_NAME >> .env
-echo DJANGO_LOG_FILE="/home/ubuntu/logs/django-$APPLICATION_NAME-app.log" >> .env
+echo DJANGO_LOG_FILE="/home/ubuntu/logs/django-app.log" >> .env
 #echo ALLOWED_HOSTS="127.0.0.1 $(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)" >> .env
 echo ALLOWED_HOSTS="*" >> .env
