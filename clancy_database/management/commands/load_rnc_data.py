6da37a9ce2d44587a1f65eca4127effefc8817f2
Arthur Barrett <abarrett@fas.harvard.edu>
2020-10-20 22:03:49 -0400

    Import RNC and Sharoff frequency data efficiently.

--- clancy_database/management/commands/load_rnc_data.py
+++ clancy_database/management/commands/load_rnc_data.py
@@ -1,52 +1,79 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 import argparse
 import csv
 import os
 import sys
+import sqlite3
 
 import clancy_database
-from clancy_database.models import Inflection
 
 
 class Command(BaseCommand):
     help = '''
-Loads inflection frequencies queried from the RNC.
+Loads frequency list of word forms obtained from the RNC.
 
-This command should only be run once the database has been populated
-with the core set of lemmas and word forms, as it only updates
-existing word forms.
+This command should only be run after the database has been populated
+with the core set of lemmas and word forms.
+
+It works by bulk inserting the RNC data into a temporary table,
+and then joining with the target table using an UPDATE/SELECT, which 
+is more efficient than a series of UPDATE queries (e.g. UPDATE x WHERE y = ?).
 '''
 
     def add_arguments(self, parser):
-        # change csv file
-        default_csv_file = os.path.join(os.path.dirname(clancy_database.__file__), "data", "demo_with_split_csv.csv")
+        default_tsv_file = os.path.join(os.path.dirname(clancy_database.__file__), "data", "rnc_freq_list.tsv")
         parser.add_argument("--file", 
             required=False, 
             type=argparse.FileType("r"), 
-            help="Input frequency list in CSV format with headers: form, docs, occurrences", 
-            default=default_csv_file)
+            help="Input frequency list in TSV format with headers: Rank, Freq, WordForm", 
+            default=default_tsv_file)
+        parser.add_argument("--dbfile", 
+            required=False, 
+            type=str,
+            help="Sqlite database", 
+            default=settings.DATABASES['clancy_database']['NAME'])
 
     def handle(self, *args, **options):
-        self.load_data(csv_file=options['file'], verbosity=options['verbosity'])
+        self.load_data(tsv_file=options['file'], dbfile=options['dbfile'], verbosity=options['verbosity'])
 
-    def load_data(self, csv_file, verbosity=0):
+    def load_data(self, tsv_file, dbfile, verbosity=0):
         total_records = 0
-        total_updated = 0
-        total_not_found =  0
+        connection = sqlite3.connect(dbfile)
+        self.cursor = connection.cursor()
 
-        csv_reader = lambda f: csv.DictReader(f, dialect=None, quoting=csv.QUOTE_NONE)
-        for record in csv_reader(csv_file):
-            word_form, docs, occurrences = record['form'], record['docs'], record['occurrences']
-            if docs != '' and occurrences != '':
-                num_updated = Inflection.objects.filter(form=word_form).update(rnc_doc_count=docs, rnc_form_count=occurrences)
-                if num_updated == 0:
-                    total_not_found += 1 
-                total_records += 1
-                total_updated += num_updated
+        self.create_table()
+        tsv_reader = lambda f: csv.DictReader(f, dialect=None, delimiter='\t', quoting=csv.QUOTE_NONE)
+        for record in tsv_reader(tsv_file):
+            total_records += 1
+            self.insert_form(record['form'], record['docs'], record['occurrences'])
+        self.join_records()
+        self.delete_table()
+        connection.commit()
+        connection.close()
+        self.stdout.write(self.style.SUCCESS(f"Loaded {total_records} records from RNC frequency data"))
 
-            if verbosity > 1:
-                if num_updated == 0:
-                    self.stderr.write(self.style.WARNING(f"RNC inflection «{word_form}» not found in database"))
-                else:
-                    self.stdout.write(f"RNC inflection «{word_form}» found in database. Updated {num_updated} objects with doc and word freqquency: {docs},{occurrences}")
-        self.stdout.write(self.style.SUCCESS(f"Loaded {total_records - total_not_found} of {total_records} records from RNC frequency list"))
+    def create_table(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS rnc_freq_list (form TEXT PRIMARY KEY, rnc_doc_count INTEGER, rnc_form_count INTEGER)")
+        self.cursor.execute("DELETE FROM rnc_freq_list")
+
+    def delete_table(self):
+        self.cursor.execute("DROP TABLE rnc_freq_list")
+
+    def insert_form(self, form, docs, occurrences):
+        sql = "INSERT INTO rnc_freq_list (form, rnc_doc_count, rnc_form_count) VALUES (?, ?, ?)"
+        values = [form, docs, occurrences]
+        try:
+            self.cursor.execute(sql, values)
+        except sqlite3.Error as e:
+            print(e)
+            print(sql, values)
+            raise e
+    
+    def join_records(self):
+        sql = """
+UPDATE inflection SET 
+    rnc_doc_count = (select rnc_doc_count from rnc_freq_list where rnc_freq_list.form = inflection.form),
+    rnc_form_count = (select rnc_form_count from rnc_freq_list where rnc_freq_list.form = inflection.form)
+"""
+        self.cursor.execute(sql)
