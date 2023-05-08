5e3f85c0ad88a6a257a1d1d1c6f2b352dc2e414b
Paige Lee <paigelee@Paiges-MacBook-Air.local>
2023-05-08 11:14:31 -0400

    similarity pan and zoom

--- parser_tool/static/js/src/navec.js
+++ parser_tool/static/js/src/navec.js
@@ -9,8 +9,8 @@
         $("#graph").empty();
         $('#slider-step').empty();
         $('.timing').text(' ')
-        $('#error').css('display','none')     
-        $('.hide').css('display','none')
+        $('#error').css('display', 'none')
+        $('.hide').css('display', 'none')
 
         var input_text = $("#contentinput").val();
         var jqXhr;
@@ -27,21 +27,21 @@
 
         jqXhr.then(function (result, textStatus) {
             console.log('result:', result);
-            if (result.status  == 'error') {
-                $('#error').css('display','block')
-                $('.loading').css('display','none')
+            if (result.status == 'error') {
+                $('#error').css('display', 'block')
+                $('.loading').css('display', 'none')
                 return false;
-            } 
-            $('.hide').css('display','block')
+            }
+            $('.hide').css('display', 'block')
 
             $('#title').text('Most similar word forms to ' + input_text)
-            $('.timing').text('in ' + result.timing.toFixed(2) +' seconds')
+            $('.timing').text('in ' + result.timing.toFixed(2) + ' seconds')
 
             drawChart(result)
 
             d3.select('#slider-step').append('div')
                 .text('Show/hide words:')
-                .style('display','inline-block')
+                .style('display', 'inline-block')
 
             // Step
             sliderStep = d3
@@ -54,19 +54,20 @@
                 .default(20)
                 .on('onchange', val => {
                     $('#graph-title').text('2-D Representation of the closest ' + val + ' forms to ' + input_text)
-
-                    circle.each(function(thiscirc, i) {
-                        if (i <= val) {
-                            d3.select(this).transition().style('opacity','1')
+                    circle.each(function () {
+                        const rank = +d3.select(this).attr('data-rank');
+                        if (rank <= val) {
+                            d3.select(this).transition().attr('display', 'block');
                         } else {
-                            d3.select(this).transition().style('opacity','0')
+                            d3.select(this).transition().attr('display', 'none');
                         }
                     })
-                    label.each(function(thislab, i) {
-                        if (i <= val) {
-                            d3.select(this).transition().style('opacity','1')
+                    label.each(function () {
+                        const rank = +d3.select(this).attr('data-rank');
+                        if (rank <= val) {
+                            d3.select(this).transition().attr('display', 'block');
                         } else {
-                            d3.select(this).transition().style('opacity','0')
+                            d3.select(this).transition().attr('display', 'none');
                         }
                     })
                 });
@@ -88,12 +89,12 @@
             //     .text('Reset zoom')
 
             $('#graph-title').text('2-D Representation of the closest ' + sliderStep.value() + ' forms to ' + input_text)
-            $('#graph-descript').text('Pan rectangle on graph to zoom in. Double-click to zoom out. Adjust slider to show and hide forms on graph.')
-            $('#header1').css('display','block')
-            $('#header2').css('display','block')
+            $('#graph-descript').text('Use mouse wheel or two finger zoom on mousepad to zoom in & out. Use top slider to adjust # of words shown on graph.')
+            $('#header1').css('display', 'block')
+            $('#header2').css('display', 'block')
 
             result.output.forEach((elt, i) => {
-                $('#output0').append('<span id="result' + (i + 1) + '" style="opacity: 1">' + (i+1) + '</span>')
+                $('#output0').append('<span id="result' + (i + 1) + '" style="opacity: 1">' + (i + 1) + '</span>')
                 $('#output1').append('<span id="result' + (i + 1) + '" style="opacity: 1">' + elt.word + '</span>')
                 $('#output2').append('<span id="sim' + (i + 1) + '" style="opacity: 1">' + elt.similarity.toFixed(4) + '</span>')
             });
@@ -139,6 +140,10 @@
         var margin = { top: 20, right: 20, bottom: 30, left: 30 };
         width = 700 - margin.left - margin.right, height = 480 - margin.top - margin.bottom;
 
+        let xTranslateCurrent = 0;
+        let yTranslateCurrent = 0;
+
+
         var tooltip = d3.select("#graph").append("div")
             .attr("class", "tooltip")
             .style("opacity", 0);
@@ -151,18 +156,26 @@
             .domain([yMin - 1, yMax + 1])
             .range([height, 0]);
 
-        var xAxis = d3.axisBottom(x).ticks(12),
-            yAxis = d3.axisLeft(y).ticks(12 * height / width);
+        var xAxis = d3.axisBottom(x).ticks(12).tickFormat(() => ''),
+            yAxis = d3.axisLeft(y).ticks(12 * height / width).tickFormat(() => '');
 
-        var brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
-            idleTimeout,
-            idleDelay = 350;
+        // var brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
+        //     idleTimeout,
+        //     idleDelay = 350;
 
         var svg = d3.select("#graph").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Define the zoom behavior
+        var zoomBehavior = d3.zoom()
+            .scaleExtent([0.5, 20]) // Set the minimum and maximum zoom scale
+            .on("zoom", zoomed);
+        // Apply the zoom behavior to the svg
+        svg.call(zoomBehavior);
+
 
         var clip = svg.append("defs").append("svg:clipPath")
             .attr("id", "clip")
@@ -180,34 +193,50 @@
             .data(data)
             .enter()
             .append('g')
+            .style('cursor','pointer')
 
         circle = node.append("circle")
             .attr("class", "dot")
-            .attr("r", 4)
+            .attr('data-rank', (d, i) => i)
+            .attr("r", 5)
             .attr("cx", function (d) { return x(d.x); })
             .attr("cy", function (d) { return y(d.y); })
-            .attr("opacity", function(d,i) {
+            .attr("display", function (d, i) {
                 if (i >= 20) {
-                    return 0;
-                } return .8;
+                    return 'none';
+                } return 'block';
             })
-            .attr('id', function(d,i) { "circle" + (i + 1) })
-            .style("fill", function(d,i) {
+            .attr('id', function (d, i) { "circle" + (i + 1) })
+            .style("fill", function (d, i) {
                 if (i == 0) { return "tomato" };
                 return "#4292c6";
             });
-        
+
         label = node
             .append('text')
+            .attr('data-rank', (d, i) => i)
             .attr("x", function (d) { return x(d.x) + 7; })
             .attr("y", function (d) { return y(d.y) + 7; })
-            .attr('id', function(d,i) { "label" + (i + 1) })
-            .attr('opacity', function(d,i) {
+            .attr('id', function (d, i) { "label" + (i + 1) })
+            .attr('display', function (d, i) {
                 if (i >= 20) {
-                    return 0;
-                } return 1;
+                    return 'none';
+                } return 'block';
             })
             .text(function (d, i) { return labels[i] });
+
+        node.on('mouseover', function (d, thisi) {
+            node.each(function (d, i) {
+                var thisNode = d3.select(this)
+                if (thisi == i) {
+                    thisNode.transition().style('opacity', 1)
+                } else {
+                    thisNode.transition().style('opacity', .2)
+                }
+            })
+        }).on('mouseout', function () {
+            node.transition().style('opacity', 1)
+        })
 
         // x axis
         svg.append("g")
@@ -216,59 +245,89 @@
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
 
-        svg.append("text")
-            .style("text-anchor", "end")
-            .attr("x", width)
-            .attr("y", height - 8)
-        // .text("X Label");
-
         // y axis
         svg.append("g")
             .attr("class", "y axis")
             .attr('id', "axis--y")
             .call(yAxis);
 
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "1em")
-            .style("text-anchor", "end")
-        // .text("Y Label");
+        // Define the zoom function that will be called when the zoom event occurs
+        function zoomed() {
+            // Get the current zoom transform
+            const transform = d3.event.transform;
 
-        scatter.append("g")
-            .attr("class", "brush")
-            .call(brush);
+            // Update the positions of the circles and labels based on the updated scales
+            circle.attr("cx", d => transform.applyX(x(d.x)))
+                .attr("cy", d => transform.applyY(y(d.y)));
 
-        function brushended() {
-            var s = d3.event.selection;
-            if (!s) {
-                if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-                x.domain([xMin - 1, xMax + 2])
-                y.domain([yMin - 1, yMax + 1])
-            } else {
+            label.attr("x", d => transform.applyX(x(d.x)) + 7)
+                .attr("y", d => transform.applyY(y(d.y)) + 7);
 
-                x.domain([s[0][0], s[1][0]].map(x.invert, x));
-                y.domain([s[1][1], s[0][1]].map(y.invert, y));
-                scatter.select(".brush").call(brush.move, null);
-            }
-            zoom();
+            // Update the axes with the new scales
+            svg.select(".x.axis").call(xAxis.scale(transform.rescaleX(x)));
+            svg.select(".y.axis").call(yAxis.scale(transform.rescaleY(y)));
         }
 
-        function idled() {
-            idleTimeout = null;
+        function dragged() {
+            const dx = d3.event.dx;
+            const dy = d3.event.dy;
+
+            const xTranslate = xTranslateCurrent + dx;
+            const yTranslate = yTranslateCurrent + dy;
+
+            svg.attr("transform", `translate(${xTranslate}, ${yTranslate}) scale(${currentScale})`);
+
+            xTranslateCurrent = xTranslate;
+            yTranslateCurrent = yTranslate;
+        }
+        const drag = d3.drag()
+            .on("drag", dragged);
+        svg.call(drag);
+
+        document.getElementById('reset-button').addEventListener('click', resetZoomAndDrag);
+
+        function resetZoomAndDrag() {
+            svg.transition()
+                .duration(750)
+                .call(zoomBehavior.transform, d3.zoomIdentity);
         }
 
-        function zoom() {
-            var t = scatter.transition().duration(750);
-            svg.select("#axis--x").transition(t).call(xAxis);
-            svg.select("#axis--y").transition(t).call(yAxis);
-            scatter.selectAll("circle").transition(t)
-                .attr("cx", function (d) { return x(d.x); })
-                .attr("cy", function (d) { return y(d.y); });
-            label.transition(t)
-                .attr("x", function (d) { return x(d.x) + 7; })
-                .attr("y", function (d) { return y(d.y) + 7; })
-        }
+
+        // scatter.append("g")
+        //     .attr("class", "brush")
+        //     .call(brush);
+
+        // function brushended() {
+        //     var s = d3.event.selection;
+        //     if (!s) {
+        //         if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
+        //         x.domain([xMin - 1, xMax + 2])
+        //         y.domain([yMin - 1, yMax + 1])
+        //     } else {
+
+        //         x.domain([s[0][0], s[1][0]].map(x.invert, x));
+        //         y.domain([s[1][1], s[0][1]].map(y.invert, y));
+        //         scatter.select(".brush").call(brush.move, null);
+        //     }
+        //     zoom();
+        // }
+
+        // function idled() {
+        //     idleTimeout = null;
+        // }
+
+        // function zoom() {
+        //     var t = scatter.transition().duration(750);
+        //     svg.select("#axis--x").transition(t).call(xAxis);
+        //     svg.select("#axis--y").transition(t).call(yAxis);
+        //     scatter.selectAll("circle").transition(t)
+        //         .attr("cx", function (d) { return x(d.x); })
+        //         .attr("cy", function (d) { return y(d.y); });
+        //     label.transition(t)
+        //         .attr("x", function (d) { return x(d.x) + 7; })
+        //         .attr("y", function (d) { return y(d.y) + 7; })
+        // }
+
     }
 
 
@@ -288,6 +347,7 @@
                 event.preventDefault()
             }
         });
+
     });
 
     // // Just execute "demo()" in the console to populate the input with sample HTML.
